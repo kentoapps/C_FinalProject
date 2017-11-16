4241a6949f4953d9e33d3daecb170bc5e02f3fcb
Kento Uchida <kento.apps@gmail.com>
2017-11-15 19:12:35 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject.dir/main.c.o"
+  "CMakeFiles/FinalProject.dir/printer.c.o"
+  "FinalProject.pdb"
+  "FinalProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
